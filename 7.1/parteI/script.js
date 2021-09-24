3e41f1194d2a5fc09cde557ae38f68a6ea69e375
tonistorres <sisvenda2011@gmail.com>
2021-09-23 21:21:38 -0300

    exercicio 7.2 parte I respondendo 1 e 2

---
+++ 7.1/parteI/script.js
@@ -0,0 +1,23 @@
+/* 
+Faça as modificações necessárias na função para que o seu 
+comportamento respeite o escopo no qual cada variável foi declarada.
+
+1- Modifique a estrutura da função para que ela seja uma arrow function 
+2- Modifique as concatenações para template literals .
+
+*/
+const funcao = (escopo) => {
+
+    if (escopo === true) {
+        let ifScope = "";
+        ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+        ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+        return console.log(ifScope);
+    } else {
+        elseScope = `Não devo ser utilizada fora meu escopo (else)`;
+        return console.log(elseScope);
+    }
+    //   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+}
+
+funcao(false);
