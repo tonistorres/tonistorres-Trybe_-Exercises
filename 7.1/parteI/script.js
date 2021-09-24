efb9b358c733701065328c0f280e2fe38f6fb438
tonistorres <sisvenda2011@gmail.com>
2021-09-24 00:25:13 -0300

    Respondendo Exercicios 7.1

--- 7.1/parteI/script.js
+++ 7.1/parteI/script.js
@@ -5,19 +5,32 @@ comportamento respeite o escopo no qual cada variável foi declarada.
 1- Modifique a estrutura da função para que ela seja uma arrow function 
 2- Modifique as concatenações para template literals .
 
+Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
+Utilize template literals para que a chamada console.log(<seu código>oddsAndEvens<seu código>);
+retorne "Os números 2,3,4,7,10,13 se encontram ordenados de forma crescente!".
+
+
 */
-const funcao = (escopo) => {
+// const funcao = (escopo) => {
 
-    if (escopo === true) {
-        let ifScope = "";
-        ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
-        ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-        return console.log(ifScope);
-    } else {
-        elseScope = `Não devo ser utilizada fora meu escopo (else)`;
-        return console.log(elseScope);
-    }
-    //   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+//     if (escopo === true) {
+//         let ifScope = "";
+//         ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+//         ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//         return console.log(ifScope);
+//     } else {
+//         elseScope = `Não devo ser utilizada fora meu escopo (else)`;
+//         return console.log(elseScope);
+//     }
+//     //   console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+// }
+
+// funcao(false);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+function myFunction1() {
+
+    return `Resultado ${oddsAndEvens.sort((a, b) => a - b)}`;
 }
-
-funcao(false);
+console.log(myFunction1());
