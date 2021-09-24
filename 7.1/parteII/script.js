b5f0512d97f5ef3ff11ef307a6064e8fac6fcdc3
tonistorres <sisvenda2011@gmail.com>
2021-09-24 11:26:48 -0300

    Respondendo Exercicios 7.1 Parte 2

--- 7.1/parteII/script.js
+++ 7.1/parteII/script.js
@@ -1,6 +1,8 @@
 /******************************************************************
  * Contribuição do canal:TecNoz by Daniel Cukier
  * Fontes de Pesquisas: https://www.youtube.com/watch?v=H5u1dOoCPnc
+ * https://pt.stackoverflow.com/questions/309073/como-mudar-todas-as-palavras-selecionadas-de-uma-s%C3%B3-vez-no-vscode
+ * https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_split
  ******************************************************************* 
  * Crie uma função que receba um número e retorne seu fatorial.
  *  Sobre o Fatorial Matematicamente Falando
@@ -31,3 +33,30 @@
 
 // }
 // console.log(`${fatorialRecursiva(8)}`);
+
+
+function recebeFrase(frase) {
+
+    let contador = 0;
+    let recebeMaiorPalavra = "";
+    const myArray = frase.split(" ");
+
+    console.log("TODAS AS FRASES:" + myArray);
+
+    for (let i = 0; i < myArray.length; i += 1) {
+        console.log('Frase[' + i + ']=' + myArray[i])
+        console.log('Tamanho:' + myArray[i].length)
+        console.log('--------------------------');
+
+        if (myArray[i].length > contador) {
+            contador += myArray[i].length;
+            recebeMaiorPalavra = myArray[i];
+        }
+
+    }
+
+    return recebeMaiorPalavra;
+
+}
+
+console.log("Essa é a Frase Maior Retornada: " + recebeFrase('Tonis Alberto Torres Ferreira'))
