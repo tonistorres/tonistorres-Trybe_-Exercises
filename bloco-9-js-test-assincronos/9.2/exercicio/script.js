0dbe2a674b5fe91eb9cd391655c32caa43715419
tonistorres <sisvenda2011@gmail.com>
2021-10-19 10:32:37 -0300

    Respondendo exercicios course

--- bloco-9-js-test-assincronos/9.2/exercicio/script.js
+++ bloco-9-js-test-assincronos/9.2/exercicio/script.js
@@ -9,17 +9,27 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  const myObject = {
-    method: 'GET',
-    headers: { 'Accept': 'application/json' }
-  };
-
-  fetch(API_URL, myObject) // nesse ponto recebemos como resposta uma promisse 
-    .then(response => response.json()) // nesse ponto iremos usar o método json na resposta da api que irá 
-                                       // converter o conteúdo body da resposta e retornar uma outra promisse
-                                       // que, quando, bem sucedida retorna um joson contendo as informações 
-                                       // da piada. 
-    .then(data => console.log(data));  // nesse ponto o retono já é os dados em formato json 
+    const myObject = {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json'
+        }
+    };
+    //***************************************************************************************************** */
+    // Documentação:https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
+    // AULA AO VIVO(Aula 9.2 - Tribo 16 -B): Apartir de 35:07  
+    // * Quando você tem um tem uma Promisse você tem três estados a ser esperados: 
+    // pendente: estado inicial, nem cumprido nem rejeitado.
+    // cumprido: significa que a operação foi concluída com sucesso. Utilizamos o método then se tudo ocorreu bem
+    // rejeitado: significa que a operação falhou. Utilizamos o método de captura de erro catch
+    //*************************************************************************************************** */
+    fetch(API_URL, myObject) // nesse ponto recebemos como resposta uma promisse 
+        .then(response => response.json()) // Então quando eu receber o resultado positiovo irei usar o método
+        // json na resposta da api que irá converter o conteúdo body da resposta e retornar uma outra promisse
+        // que, quando, bem sucedida retorna um joson contendo as informações 
+        // da piada.    
+        .then(data => console.log(data)); // aqui temos o dados da promisse da promisse e finalmente o conteudo 
+                                         // json   
 };
 
 window.onload = () => fetchJoke();
