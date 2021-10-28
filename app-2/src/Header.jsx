91f1bddc8cdeec6e20339833ed2e5fb8a762bfd6
tonistorres <sisvenda2011@gmail.com>
2021-10-28 10:02:16 -0300

    exercicio 1 Respondido

---
+++ app-2/src/Header.jsx
@@ -0,0 +1,12 @@
+import React, {
+    Component
+} from "react";
+
+
+class Header extends Component {
+  render() {
+     return  <h1 className="component-rodape-cabecalho">Conteúdos de Front-End</h1>
+  }
+}
+
+export default Header;
